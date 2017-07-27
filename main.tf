d67f31e60a459259c4d7796d7191e3f5d6f34cda
dannymacnny <!12358dm97832>
2017-07-26 19:32:13 -0600

    Formatting and CIDR fix
    Added the vpc security group to the aws_instance resource and fixed the CIDR notation in the aws_security_group resource. Then just added a few returns to break things up.

--- main.tf
+++ main.tf
@@ -4,11 +4,14 @@ region = "us-west-2"
 resource "aws_instance" "example"{
   ami = "ami-835b4efa"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "${aws_security_group.instance.id}" ]
+
   user_data = <<-EOF
     #!/bin/bash
     echo "Hello World">index.html
     nohup busybox httpd -f 0p 8080 &
     EOF
+
   tags{
     Name = "terraform-example"
   }
@@ -16,11 +19,10 @@ resource "aws_instance" "example"{
 
 resource "aws_security_group" "instance"{
   name = "terraform-example-instance"
-
   ingress {
     from_port = 8080
     to_port = 8080
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
